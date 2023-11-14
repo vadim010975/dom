2debd3fc0e659babab45f7705c773495e27153f2
vadim010975 <vadim010975@mail.ru>
2023-11-14 22:00:55 +0300

    14.11

--- src/js/Board.js
+++ src/js/Board.js
@@ -5,7 +5,6 @@ export default class Board {
   }
 
   init() {
-    console.log("init");
     for (let i = 0; i < Math.pow(this.size, 2); i += 1) {
       const cellEl = document.createElement("div");
       cellEl.classList.add("cell");
